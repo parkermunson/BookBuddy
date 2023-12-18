d9373ef632e66352d95b05a3e1b15c451822109c
parkermunson <parkermunson@gmail.com>
2023-12-18 09:27:17 -0500

    style homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -9,20 +9,17 @@ const Homepage = () => {
         <div>
             {user ? <h3 id="welcome-message">Welcome fellow reader!</h3> : null}
         <div className="homepage-image-container">
-            
         <img className="homepage-image" src="src/assets/homepageImg.jpeg" alt="Homepage" />
-
-
+        </div>
+        <div>
             <h1 id="tagline">BOOKS YOU PROBABLY SHOULD HAVE READ BY NOW</h1>
-            
             <p id="textbox">It's never too late to start reading the books<br />you've lied about having already read.</p>
-            
+            </div>
             <div className="read-button-container">
                 <button className="read-button">
                     <Link to='/books'>START READING</Link>
                 </button>
             </div>
-        </div>
         </div>
     )
 }
